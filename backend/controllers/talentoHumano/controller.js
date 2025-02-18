3e2e2b995a3aed6316279afc0a933b72a2d6cc3b
roncortez <ronny.cortez12@gmail.com>
2025-02-17 23:37:24 -0500

    Update controller, model, route

--- backend/controllers/talentoHumano/controller.js
+++ backend/controllers/talentoHumano/controller.js
@@ -1,21 +1,24 @@
-const empleadoModel = require('../../models/talentoHumano/empleadoModel');
+const model = require('../../models/talentoHumano/model');
 
-const empleadoController = {
+const controller = {
+
+    //EMPLEADOS
     getEmpleados: async (req, res) => {
-        const result = await empleadoModel.getEmpleados();
+        const result = await model.getEmpleados();
         return res.json(result);
     },
 
+    // PERMISOS
     createPermiso: async(req, res) => {
         const data = req.body;
-        const result = await empleadoModel.createPermiso(data);
+        const result = await model.createPermiso(data);
         return res.json(result);
     },
 
     obtenerPermisoPorId: async(req, res) => {
         try {
             const id = req.params.id;
-            const result = await empleadoModel.obtenerPermisoPorId(id);
+            const result = await model.obtenerPermisoPorId(id);
             return res.json(result);
 
         } catch (error) {
@@ -24,18 +27,23 @@ const empleadoController = {
     },
 
     obtenerUltimoPermiso: async(req, res) => {
-        const result = await empleadoModel.obtenerUltimoPermiso();
+        const result = await model.obtenerUltimoPermiso();
         return res.json(result);
     },
 
     obtenerPermisos: async(req, res) => {
         try {
-            const result = await empleadoModel.obtenerPermisos();
+            const result = await model.obtenerPermisos();
             return res.json(result);
         } catch (error) {
             console.error("Error en el controlador: ", error);
         }
+    }, 
+
+    getReport: async (req, res) => {
+        const result = await model.getReport(req.body);
+        return res.json(result);
     }
 }
 
-module.exports = empleadoController;
+module.exports = controller;
